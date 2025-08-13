04fa11bc16119bec15ec5944e51983408288681b
Carlos Pulido Rosas <carlos.pulido.rosas@gmail.com>
2025-08-12 21:28:28 -0600

    Update CSV file path for data loading
    Changed the CSV file path in Histogramas_Python.py to a relative path for improved portability and easier access across different environments.

--- Sesion 1/Histogramas_Python.py
+++ Sesion 1/Histogramas_Python.py
@@ -2,9 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 1. Load CSV file
-df = pd.read_csv(
-    r"C:\Users\carlo\OneDrive\Documentos\Maestría en Ciencia de los Datos\Semestre 1\PROGRAMACIÓN 1\Sesion 1 (12-08-2025)\categoria_de_corredores.csv"
-)
+df = pd.read_csv("Sesion 1/categorias_de_corredores.csv")
+
 # 2. Check DataFrame info
 df.info()
 df.head()
